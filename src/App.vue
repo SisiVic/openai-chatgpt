a520e11ef4e7ad73d70d17ba5f61553ef81fe97e
victor <victorayala@sisidev.com>
2022-12-16 10:29:40 -0800

    Tags implementation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
-<script setup>
-import Chat from './components/IndexChat.vue'
-</script>
 
 <template>
   <Chat />
 </template>
+
+<script>
+import { defineComponent } from '@vue/runtime-core'
+import Chat from './components/IndexChat.vue'
+
+export default defineComponent ({
+  components: {
+    Chat
+  }
+});
+
+
+</script>
 
 <style scoped>
 
--- src/components/IndexChat.vue
+++ src/components/IndexChat.vue
@@ -1,5 +1,23 @@
 <template>
-
+  <div class="IndexChat">
+    <section class="IndexChat-conversation">
+      <div class="IndexChat-userAnswer">
+        <img src="" alt="">
+        <p>Hola</p>
+        <div class="IndexChat-icons">
+        </div>
+      </div>
+      <div class="IndexChat-chatAnswer">
+        <img src="" alt="">
+        <p>Hola! ¿Cómo estás hoy? ¿Hay algo en particular en lo que pueda ayudarte?</p>
+        <div class="IndexChat-icons">
+        </div>
+      </div>
+    </section>
+    <section class="IndexChat-input">
+      
+    </section>
+  </div>
 </template>
 
 <script setup>
@@ -7,5 +25,27 @@
 </script>
 
 <style scoped>
+.IndexChat-conversation {
+
+}
+.IndexChat-conversation{
+
+}
+
+.IndexChat-input {
+
+}
+
+.IndexChat-userAnswer {
+  background-color: rgb(68, 67, 67);
+}
+
+.IndexChat-chatAnswer {
+  background-color: rgb(124, 123, 123);
+}
+
+.IndexChat-icons {
+
+}
 
 </style>
